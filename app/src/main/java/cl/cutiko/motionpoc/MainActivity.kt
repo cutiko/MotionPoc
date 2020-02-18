b73feebd4a7e18d739e3adae56db5f8024c3b1f3
Erick Navarro <cutiko@gmail.com>
2020-02-18 17:22:25 -0300

    POC for sticky header overlapping a middle header

--- app/src/main/java/cl/cutiko/motionpoc/MainActivity.kt
+++ app/src/main/java/cl/cutiko/motionpoc/MainActivity.kt
@@ -1,8 +1,13 @@
 package cl.cutiko.motionpoc
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +15,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        backGrounderButton.setOnClickListener {
-            with(immutableView) {
-                visibility = if (visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        with(recycler) {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = cl.cutiko.motionpoc.MainActivity.PseudoAdapter()
+        }
+
+    }
+
+    class PseudoHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun setText(position: String) {
+            (itemView as? TextView)?.text = position
+        }
+    }
+
+    class PseudoAdapter : RecyclerView.Adapter<PseudoHolder>() {
+
+        private val list = makeList()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PseudoHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(android.R.layout.simple_list_item_1, parent, false)
+            return PseudoHolder(view)
+        }
+
+        override fun getItemCount() = list.size
+
+        override fun onBindViewHolder(holder: PseudoHolder, position: Int) {
+            val text = list[position]
+            holder.setText(text)
+        }
+
+        companion object {
+            private fun makeList(): List<String> {
+                val mutableList = mutableListOf<String>()
+                for (i  in 0 .. 100) {
+                    mutableList.add(i.toString())
+                }
+                return mutableList
             }
         }
     }
