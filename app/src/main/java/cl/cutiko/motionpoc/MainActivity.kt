c6c2d4c4c6891ec099cb63498d149205d37734aa
Erick Navarro <cutiko@gmail.com>
2020-02-18 15:09:19 -0300

    usage of setters in views

--- app/src/main/java/cl/cutiko/motionpoc/MainActivity.kt
+++ app/src/main/java/cl/cutiko/motionpoc/MainActivity.kt
@@ -1,12 +1,19 @@
 package cl.cutiko.motionpoc
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        backGrounderButton.setOnClickListener {
+            with(immutableView) {
+                visibility = if (visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+            }
+        }
     }
 }
